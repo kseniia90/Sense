8a865c168420d1560493d319b75539e095a29289
Kseniia Pukhova <xenio310390@gmail.com>
2024-07-03 22:02:33 +0300

    popup fix

--- page2/popup.js
+++ page2/popup.js
@@ -1,18 +1,15 @@
-if (document.querySelector('.thanks_popup') !== null) {
+if (document.querySelector('.winn_popup') !== null) {
 
-    document.querySelectorAll('.open_thanks_popup').forEach(function (button) {
+    window.setTimeout(function() {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('.winn_popup-form').classList.add('active');
+    }, 6000);
+
+    document.querySelectorAll('.winn_popup .close_popup').forEach(function (button) {
         button.addEventListener('click', function (e) {
-            e.preventDefault();
-            document.body.style.overflow = 'hidden';
-            document.querySelector('.thanks_popup').classList.add('active');
+            document.body.style.overflow = 'visible';
+            document.querySelector('.winn_popup-form').classList.remove('active');
+            document.querySelector('.winn_popup-thanks').classList.remove('active');
         });
     })
-    document.querySelector('.thanks_popup-content').addEventListener('click', function (e) {
-        e.stopPropagation();
-    });
-
-    document.querySelector('.thanks_popup .close_popup').addEventListener('click', function (e) {
-        document.body.style.overflow = 'visible';
-        document.querySelector('.thanks_popup').classList.remove('active');
-    });
-} 
+};
